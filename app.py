77c4a1735e634ad8058f1a620e1c532c33a196ec
Abeer <abeeralbalushia12@gmail.com>
2026-06-03 22:11:13 +0400

    fix feedback error

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.secret_key = "starcare_secret"
 
 # ---------------- MONGO ----------------
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb+srv://abeeralbalushia12_db_user:NP2h8oAmV1WCbC9e@cluster0.iszw6m4.mongodb.net/starcare_feedback?retryWrites=true&w=majority")
 db = client["starcare_feedback"]
 collection = db["feedback"]
 
@@ -220,4 +220,4 @@ def qr_dashboard():
 # ---------------- RUN ----------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)  
