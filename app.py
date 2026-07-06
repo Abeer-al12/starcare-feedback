46f82858eb43ec52584638a216878f6f883dbbb6
Abeer <abeeralbalushia12@gmail.com>
2026-07-06 22:45:59 +0400

    fix|

--- app.py
+++ app.py
@@ -1496,6 +1496,11 @@ def download_excel():
 
     data = list(collection.find(query))
 
+
+    if data:
+    print("created_at =", data[0].get("created_at"))
+    print("type =", type(data[0].get("created_at")))
+
     # ===========================
     # Excel file
     # ===========================
